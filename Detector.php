80f0088571ca014b1b9d353c901b6af673435ec6
Tom W Hartung <tomwhartung@gmail.com>
2015-06-09 12:26:56 -0600

    Made Detector.php a class and verified it works OK.

--- Detector.php
+++ Detector.php
@@ -1,46 +1,69 @@
 <?php
-//
-// idMyGadget device detection:
-//
-define( 'DETECT_MOBILE_BROWSERS', 'detect_mobile_browsers' );
-define( 'MOBILE_DETECT',  'mobile_detect' );
-define( 'TERA_WURFL', 'tera_wurfl' );
+/**
+ * Creates an object of the desired idMyGadget subclass and uses it for device detection.
+ */
 
-//
-$gadgetDetector = DETECT_MOBILE_BROWSERS;
-// $gadgetDetector = MOBILE_DETECT;
-// $gadgetDetector = TERA_WURFL;
-
-$idMyGadget = null;
-$debugging = FALSE;
-$allowOverridesInUrl = TRUE;
-$cwd = getcwd();
-$dir_to_add = $cwd . '/templates/jmws_protostar_idmygadget/jmws_idMyGadget_for_joomla';
-set_include_path( get_include_path() . PATH_SEPARATOR . $dir_to_add );
-
-if ( $gadgetDetector === DETECT_MOBILE_BROWSERS )
+class Detector
 {
-	require_once 'gadget_detectors/detect_mobile_browsers/php/detectmobilebrowser.php';     // sets $usingMobilePhone global variable
-	require_once 'php/IdMyGadgetDetectMobileBrowsers.php';
-	$idMyGadget = new IdMyGadgetDetectMobileBrowsers( $debugging, $allowOverridesInUrl );
-}
-else if ( $gadgetDetector === MOBILE_DETECT )
-{
-	require_once 'gadget_detectors/mobile_detect/Mobile-Detect/Mobile_Detect.php' ;
-	require_once 'php/IdMyGadgetMobileDetect.php';
-	$idMyGadget = new IdMyGadgetMobileDetect( $debugging, $allowOverridesInUrl );
-}
-else if ( $gadgetDetector === TERA_WURFL )
-{
-	require_once 'gadget_detectors/tera_wurfl/Tera-Wurfl/wurfl-dbapi/TeraWurfl.php';
-	require_once 'php/IdMyGadgetTeraWurfl.php';
-	$idMyGadget = new IdMyGadgetTeraWurfl( $debugging, $allowOverridesInUrl );
-}
+	const DETECT_MOBILE_BROWSERS = 'detect_mobile_browsers';
+	const MOBILE_DETECT = 'mobile_detect';
+	const TERA_WURFL = 'tera_wurfl';
+	/**
+	 * The gadget detector that we are using
+	 */
+	public $gadgetDetector = self::DETECT_MOBILE_BROWSERS;  // default is to use the small, minimal one
+	/**
+	 * The idMyGadget object we are using
+	 */
+	public $idMyGadget = null;
+	/**
+	 * The device data we get from the detector
+	 */
+	public $deviceData = null;
+	/**
+	 * A string that represents the gadget being used
+	 */
+	public $gadgetString = "";
 
-if ( $idMyGadget !== null )
-{
-	require_once 'gadget_detectors/all_detectors/getGadgetString.php';
-	$deviceData = $idMyGadget->getDeviceData();
-	$gadgetString = getGadgetString( $deviceData );
-}
+	/**
+	 * Constructor: for best results, specify a different gadgetDetector
+	 */
+	public function __construct( $gadgetDetector=null, $debugging=FALSE, $allowOverridesInUrl=TRUE )
+	{
+		if ( $gadgetDetector === null )
+		{
+			$gadgetDetector = self::DETECT_MOBILE_BROWSERS;  // default is to use the small, minimal one
+		}
 
+		$this->gadgetDetector = $gadgetDetector;
+		$cwd = getcwd();
+		$dir_to_add = $cwd . '/templates/jmws_protostar_idmygadget/jmws_idMyGadget_for_joomla';
+		set_include_path( get_include_path() . PATH_SEPARATOR . $dir_to_add );
+
+		if ( $gadgetDetector === self::DETECT_MOBILE_BROWSERS )
+		{
+			require_once 'gadget_detectors/detect_mobile_browsers/php/detectmobilebrowser.php';     // sets $usingMobilePhone global variable
+			require_once 'php/IdMyGadgetDetectMobileBrowsers.php';
+			$this->idMyGadget = new IdMyGadgetDetectMobileBrowsers( $debugging, $allowOverridesInUrl );
+		}
+		else if ( $gadgetDetector === self::MOBILE_DETECT )
+		{
+			require_once 'gadget_detectors/mobile_detect/Mobile-Detect/Mobile_Detect.php' ;
+			require_once 'php/IdMyGadgetMobileDetect.php';
+			$this->idMyGadget = new IdMyGadgetMobileDetect( $debugging, $allowOverridesInUrl );
+		}
+		else if ( $gadgetDetector === self::TERA_WURFL )
+		{
+			require_once 'gadget_detectors/tera_wurfl/Tera-Wurfl/wurfl-dbapi/TeraWurfl.php';
+			require_once 'php/IdMyGadgetTeraWurfl.php';
+			$this->idMyGadget = new IdMyGadgetTeraWurfl( $debugging, $allowOverridesInUrl );
+		}
+
+		if ( $this->idMyGadget !== null )
+		{
+			require_once 'gadget_detectors/all_detectors/getGadgetString.php';
+			$this->deviceData = $this->idMyGadget->getDeviceData();
+			$this->gadgetString = getGadgetString( $this->deviceData );
+		}
+	}
+}
