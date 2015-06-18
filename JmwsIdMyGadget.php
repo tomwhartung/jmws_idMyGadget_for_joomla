632175cb630600ea6832014ca7f8860a95de57ad
Tom W Hartung <tomwhartung@gmail.com>
2015-06-17 18:04:03 -0600

    Fixed Strict Standards warning.

--- JmwsIdMyGadget.php
+++ JmwsIdMyGadget.php
@@ -50,7 +50,7 @@ class JmwsIdMyGadget
 	 */
 	public function __construct( $gadgetDetector=null, $debugging=FALSE, $allowOverridesInUrl=TRUE )
 	{
-		$application = &JFactory::getApplication();
+		$application = JFactory::getApplication();
 		$templateName = $application->getTemplate();
 		$idMyGadgetDir = JPATH_THEMES . DS . $templateName . DS . 'jmws_idMyGadget_for_joomla';
 		set_include_path( get_include_path() . PATH_SEPARATOR . $idMyGadgetDir );
