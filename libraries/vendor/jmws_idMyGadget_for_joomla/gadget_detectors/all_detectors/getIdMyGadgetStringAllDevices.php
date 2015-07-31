7dc5334d809b198b1fc5fb7e3e4d5f93e8750054
Tom W Hartung <tomwhartung@gmail.com>
2015-07-31 14:49:39 -0600

    Renamed the function to match the filename, which was rather the point of renaming the file.  Updated to use the new GADGET_STRING_* constants in class IdMyGadget.  Also updated to match the functionality formerly in class JmwsIdMyGadget, but that I moved here, so that we can access it from the demo programs, which we may not want to necessarily use for demos, but which can help with troubleshooting someday, and since we already wrote them, well I guess we should get them to work, without adding a bunch of hacks and redundancy.

--- libraries/vendor/jmws_idMyGadget_for_joomla/gadget_detectors/all_detectors/getIdMyGadgetStringAllDevices.php
+++ libraries/vendor/jmws_idMyGadget_for_joomla/gadget_detectors/all_detectors/getIdMyGadgetStringAllDevices.php
@@ -1,40 +1,28 @@
  <?php
- /**
-  * Return a short string describing the device, based on the device data passed in
-  *
-  * @param type $deviceData
-  */
-function getGadgetString( $deviceData )
-{
-	global $gadgetString;
-	$gadgetString = "Unknown Device";
+/**
+ * Return a short string describing the device, based on the device data passed in
+ *
+ * @param type $deviceData
+ */
 
+function getIdMyGadgetStringAllDevices( $deviceData )
+{
+	$gadgetString = IdMyGadget::GADGET_STRING_UNKNOWN_DEVICE;
 	$gadgetType = $deviceData["gadgetType"];
 	$gadgetModel = $deviceData["gadgetModel"];
 	$gadgetBrand = $deviceData["gadgetBrand"];
 
 	if ( $gadgetType === IdMyGadget::GADGET_TYPE_DESKTOP )
 	{
-		$gadgetString = "Desktop";
+		$gadgetString = IdMyGadget::GADGET_STRING_DESKTOP;
 	}
 	else if ( $gadgetType === IdMyGadget::GADGET_TYPE_TABLET )
 	{
-		$gadgetString = "Tablet";
+		$gadgetString = IdMyGadget::GADGET_STRING_TABLET;
 	}
 	else if ( $gadgetType === IdMyGadget::GADGET_TYPE_PHONE )
 	{
-		if ( $gadgetModel === IdMyGadget::GADGET_MODEL_APPLE_PHONE )
-		{
-			$gadgetString = "iPhone";
-		}
-		else if ( $gadgetModel === IdMyGadget::GADGET_MODEL_ANDROID_PHONE )
-		{
-			$gadgetString = "Android Phone";
-		}
-		else
-		{
-			$gadgetString = "Mobile Phone";
-		}
+		$gadgetString = IdMyGadget::GADGET_STRING_PHONE;
 	}
 	return $gadgetString;
 }
